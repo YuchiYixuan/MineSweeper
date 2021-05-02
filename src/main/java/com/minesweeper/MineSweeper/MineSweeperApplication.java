a7e5b228500f6b95eea67c5b82046b0045c31523
YuchiYixuan <83524799+YuchiYixuan@users.noreply.github.com>
2021-05-02 16:05:28 +0800

    Update MineSweeperApplication.java

--- src/main/java/com/minesweeper/MineSweeper/MineSweeperApplication.java
+++ src/main/java/com/minesweeper/MineSweeper/MineSweeperApplication.java
@@ -3,7 +3,6 @@ package com.minesweeper.MineSweeper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class MineSweeperApplication {
 
     public static void main(String[] args) {
